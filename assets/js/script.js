da1d74900704f43864f3578a6642198bb2a0e47d
chart099 <122659980+chart099@users.noreply.github.com>
2023-09-25 20:26:51 -0500

    working on highscore page with buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
+// variables for each of the pages
 var startPage = document.getElementById("start-page");
 var questionPage = document.getElementById("question-page");
 var finishedPage = document.getElementById("finished-page");
-var highschorePage = document.getElementById("highscore-page");
+var highscorePage = document.getElementById("highscore-page");
 
+// variables for the timer elements
 var timer = document.getElementById("timer");
 var timerObject;
 var timeCounter = 100;
 
+// bank of questions for quiz
 var questionBank = [
     {
         question: "insert question 1",
@@ -30,23 +33,27 @@ var questionBank = [
     }
 ];
 
+// variables for questions and answers
 var currentQuestion = 0; 
 var question = document.getElementById("question");
 var answer1 = document.getElementById("answer1");
 var answer2 = document.getElementById("answer2");
 var answer3 = document.getElementById("answer3");
 var answer4 = document.getElementById("answer4");
-
+// variable for feedback below questions
 var feedback = document.getElementById("feedback");
 
+// variable for start button
 var startButton = document.getElementById("start-quiz");
-
+// created score variable, start from zero
 var score = 0;
 
+// hiding the non-used pages from initially showing
 questionPage.style.display = "none";
 finishedPage.style.display = "none";
-highschorePage.style.display = "none";
+highscorePage.style.display = "none";
 
+// event listener that acts on start button
 startButton.addEventListener("click", function(){
     questionPage.style.display = "block";
     startPage.style.display = "none";
@@ -61,6 +68,7 @@ startButton.addEventListener("click", function(){
     displayCurrentQuestion();
 });
 
+// function that sets each question and answer as options from the question bank
 function displayCurrentQuestion() {
     question.textContent = questionBank[currentQuestion].question;
     answer1.textContent = questionBank[currentQuestion].answers[0];
@@ -70,6 +78,7 @@ function displayCurrentQuestion() {
 
 }
 
+// function that checks if the user selected the correct answer
 function checkAnswer(event) {
     var userAnswer = event.target.textContent;
     if (userAnswer === questionBank[currentQuestion].correctAnswer) {
@@ -88,8 +97,11 @@ function checkAnswer(event) {
     }
 };
 
+// variable for displaying the final score
 var finalScore = document.getElementById("final-score");
 
+
+// function that ends the questions and displays the final score
 function endGame() {
     questionPage.style.display = "none";
     finishedPage.style.display = "block";
@@ -97,15 +109,51 @@ function endGame() {
     finalScore.textContent = "Your score is: " + (timeCounter + score);
 }
 
+// event listeners for each answer button
 answer1.addEventListener("click", checkAnswer);
 answer2.addEventListener("click", checkAnswer);
 answer3.addEventListener("click", checkAnswer);
 answer4.addEventListener("click", checkAnswer);
 
+
+// variable and event listener for submitting user initials
 var submitInitials = document.getElementById("submit-initials");
 
+// function+event listener for storing the user data locally
 submitInitials.addEventListener("click", function() {
-    var user
+    var userInitials = document.getElementById("enter-initials");
+    var storeInitials = JSON.parse(localStorage.getItem("code-quiz")) || [];
+    storeInitials.push({user: userInitials.value, score:(timeCounter+score)});
+    localStorage.setItem("code-quiz", JSON.stringify(storeInitials));
+    finishedPage.style.display = "none";
+    highscorePage.style.display = "block";
+    displayHighScore()
+})
+
+
+
+var goBack = document.getElementById("go-back");
+var clearHS = document.getElementById("clear-hs");
+var scores = document.getElementById("scores");
+
+function displayHighScore() {
+    var scoreList = JSON.parse(localStorage.getItem("code-quiz"));
+    for (let i=0; i < scoreList.length; i++) {
+        var scoreItem = document.createElement("li");
+        scoreItem.textContent = scoreList[i].score+ "   " + scoreList[i].user  ;
+        
+        scores.append(scoreItem);
+    }
+ }
+
+goBack.addEventListener("click", function() {
+    finishedPage.style.display = "block";
+    highscorePage.style.display = "none";
+    scores.style.display = "none";
+})
+
+clearHS.addEventListener("click", function() {
+    localStorage.removeItem("code-quiz");
 })
 
 
